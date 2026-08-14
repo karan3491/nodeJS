bf9820bfad870d7444177f94e890ebf407704337
karan3491 <karan3491@gmail.com>
2026-08-14 10:25:37 +0530

    Enhance sortBy function to support order direction
    Added support for sorting in both ascending and descending order.

--- GenericUtilities/sortBy.ts
+++ GenericUtilities/sortBy.ts
@@ -4,3 +4,22 @@ export function sortBy<T>(
 ): T[]{
   return [...arr].sort((a,b)=> selector(a)- selector(b));
 }
+
+// with descending and ascending order
+
+export function sortBy<T>(
+    arr: T[],
+    selector: (item: T) => number,
+    descending = false
+): T[] {
+    return [...arr].sort((a, b) => {
+        const result = selector(a) - selector(b);
+        return descending ? -result : result;
+    });
+}
+
+sortBy(users, user => user.age);
+// ascending
+
+sortBy(users, user => user.age, true);
+// descending
